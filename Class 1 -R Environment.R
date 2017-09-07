446367626770a17826ad9fe1e7906e86f97f1d4f
Sinu Daniel <31669088+snoozefest@users.noreply.github.com>
2017-09-08 04:19:25 +0530

    Update Class 1 -R Environment.R

--- Class 1 -R Environment.R
+++ Class 1 -R Environment.R
@@ -28,11 +28,7 @@ getwd()
 
 ## Set the Working Directory
 
-
-
-setwd("C:\\Ram\\General 20150804 v1\\Trainings\\R Programming for Data Science")
-## Notice the direction of the slash (/)
-setwd("C:/Ram/General 20150804 v1/R Training NAB/Day 1")
+setwd("/Users/sinu/MICA/Studies/Study Material/Term 4/AMMA")
 
 getwd()
 
